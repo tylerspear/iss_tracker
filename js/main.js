5b1400ab35fb1752496b1dc97c7dd5e4f668efb7
Tyler Spear <stylerspear@gmail.com>
2022-05-31 16:13:31 -0500

    refactored code to be asynchronous and to not force window refresh any longer

--- js/main.js
+++ js/main.js
@@ -1,60 +1,51 @@
-let coords
-const url = 'http://api.open-notify.org/iss-now'
-const coordinateEl = document.getElementById('coordinates')
+const positionFeature = new ol.Feature()
 
-async function getCoords() {
-  const response = await fetch(url)
-  if(!response.ok){
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
-  }
-  let data = await response.json()
-  return data
+async function updateISSPosition() {
+  const res = await fetch("https://api.wheretheiss.at/v1/satellites/25544")
+  const data = await res.json()
+  .then(data => {
+    let ISSPosition = [data.longitude, data.latitude]
+    let ISSMarker = ol.proj.fromLonLat(ISSPosition)
+    //update the ISS image layer to be new coords from API
+    positionFeature.setGeometry(new ol.geom.Point(ISSMarker))
+    //center the view on the ISS point
+    map.getView().setCenter(ISSMarker)
+  })
 }
-
-getCoords().then(data => {
-  coords = [data.iss_position.longitude, data.iss_position.latitude]
-}).then(updateMap)
-
-
-function updateMap() {
-  updateCoords()
-  const iconFeature = new ol.Feature({
-    geometry: new ol.geom.Point(ol.proj.fromLonLat(coords)),
-    name: 'Internation Space Station',
-  });
-  
-  const map = new ol.Map({
-    target: 'map',
-    layers: [
+//paint new map
+const map = new ol.Map({
+  layers: [
     new ol.layer.Tile({
-    source: new ol.source.OSM(),
-    }),
-    new ol.layer.Vector({
-    source: new ol.source.Vector({
-      features: [iconFeature]
-    }),
-    style: new ol.style.Style({
-      image: new ol.style.Icon({
+      source: new ol.source.OSM()
+    })
+  ],
+  target: "map",
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+})
+
+//iss image layer
+positionFeature.setStyle(
+  new ol.style.Style({
+    image: new ol.style.Icon({
       anchor: [0.5, 46],
       anchorXUnits: 'fraction',
       anchorYUnits: 'pixels',
       src: 'img/iss.png'
       })
-    })
-    })
-    ],
-    view: new ol.View({
-    center: ol.proj.fromLonLat(coords),
-    zoom: 5
-    })
-  });
-}
+  })
+)
 
-function updateCoords() {
-  coordinateEl.innerHTML = `Latitude: ${coords[1]} </br> Longitude: ${coords[0]}`
-}
+new ol.layer.Vector({
+  map: map,
+  source: new ol.source.Vector({
+    features: [positionFeature]
+  })
+});
 
-setTimeout(function(){
-  window.location.reload(1);
-}, 5000);
+//initial map rendering
+updateISSPosition()
+//continually update ISS layer position
+setInterval(updateISSPosition, 3000)
